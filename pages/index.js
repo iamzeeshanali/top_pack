a852fa3fdea301c052e7fd7f237b3426ced760ac
imtiaz-abbas <imtiaz@able.do>
2022-05-12 15:20:19 +0530

    changes state logic

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,46 @@
-import Head from 'next/head'
-import {useState} from 'react'
+import Head from "next/head";
+import { useState } from "react";
 
 function HomePage() {
+  //   const [color, setColor] = useState("px-4 py-4 bg-purple-900 text-white");
 
-    const [color, setColor] = useState('px-4 py-4 bg-purple-900 text-white');
+  // home, top_pack
+  const [selectedTab, setSelectedTab] = useState("home");
+  const homeStyle = "px-4 py-4 bg-red-900 text-white";
+  const topPackStyle = "px-4 py-4 bg-green-900 text-white";
 
-    return (
-
-        <>
-        <div class="container">
+  return (
+    <>
+      <div class="container">
         <Head>
-            <title>TopPacks</title>
-            <script src="https://cdn.tailwindcss.com"></script>
+          <title>TopPacks</title>
+          <script src="https://cdn.tailwindcss.com"></script>
         </Head>
-        </div>
-        <nav class={color}>
-            <ul class="flex">
-                <li class="mx-2 cursor-pointer" onClick={()=> setColor("px-4 py-4 bg-red-900 text-white")}>HomeScreen</li>
-                <li class="mx-2 cursor-pointer" onClick={()=> setColor("px-4 py-4 bg-green-900 text-white")}>TopPacks</li>
-            </ul>
-        </nav>
-        </>
-    )
-
+      </div>
+      <nav class={selectedTab === "home" ? homeStyle : topPackStyle}>
+        <ul class="flex">
+          <li
+            class="mx-2 cursor-pointer"
+            // onClick={() => setColor("px-4 py-4 bg-red-900 text-white")}
+            onClick={() => {
+              setSelectedTab("home");
+            }}
+          >
+            HomeScreen
+          </li>
+          <li
+            class="mx-2 cursor-pointer"
+            onClick={() => {
+              setSelectedTab("top_pack");
+            }}
+            // onClick={() => setColor("px-4 py-4 bg-green-900 text-white")}
+          >
+            TopPacks
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default HomePage;
